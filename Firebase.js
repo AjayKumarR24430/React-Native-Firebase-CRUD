c4cc5e39f7e96a4a33183b1c9535e0cf0cc17c01
Ajay Kumar R <ajaykumarr3024@gmail.com>
2021-01-10 11:08:31 +0530

    firebase

--- Firebase.js
+++ Firebase.js
@@ -4,13 +4,13 @@ import firestore from 'firebase/firestore'
 const settings = {timestampsInSnapshots: true};
 
 const config = {
-    apiKey: "AIzaSyAxEru8zM4WX9MUys0L2j9SYmvsS0hwmNc",
-    authDomain: "todo-list-react-native-5703f.firebaseapp.com",
-    projectId: "todo-list-react-native-5703f",
-    storageBucket: "todo-list-react-native-5703f.appspot.com",
-    messagingSenderId: "110790818122",
-    appId: "1:110790818122:web:00db179c5b3b1ff71529b3",
-    measurementId: "G-G3E9RWZEZW"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 firebase.initializeApp(config);
 
